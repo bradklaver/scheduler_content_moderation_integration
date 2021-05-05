cefaf02c897fd1d3cfde68a2978dc9b3464ef870
Jonathan Smith <jonathan1055@sandfordsolutions.com>
2021-05-05 11:52:07 +0100

    Expand Kernel/ModeratedContentSchedulingTest to cover media entities

--- tests/src/Kernel/ModeratedContentSchedulingTest.php
+++ tests/src/Kernel/ModeratedContentSchedulingTest.php
@@ -2,8 +2,6 @@
 
 namespace Drupal\Tests\scheduler_content_moderation_integration\Kernel;
 
-use Drupal\node\Entity\Node;
-
 /**
  * Tests publishing/unpublishing scheduling for moderated nodes.
  *
@@ -13,112 +11,123 @@ class ModeratedContentSchedulingTest extends SchedulerContentModerationTestBase 
 
   /**
    * Tests moderated nodes publish scheduling.
+   *
+   * @dataProvider dataEntityTypes()
    */
-  public function testPublishStateSchedule() {
-    $node = Node::create([
-      'type' => 'example',
+  public function testPublishStateSchedule($entityTypeId, $bundle) {
+    $titleField = ($entityTypeId == 'media') ? 'name' : 'title';
+    $storage = \Drupal::service('entity_type.manager')->getStorage($entityTypeId);
+
+    $entity = $this->createEntity($entityTypeId, $bundle, [
       'title' => 'Published title',
       'moderation_state' => 'draft',
       'publish_on' => strtotime('yesterday'),
       'publish_state' => 'published',
     ]);
-    $node->save();
-    $revision_id = $node->getRevisionId();
+    $revision_id = $entity->getRevisionId();
 
     // Make sure node is unpublished.
-    $this->assertEquals(FALSE, Node::load($node->id())->isPublished());
+    $this->assertEquals(FALSE, $entity->isPublished());
 
     $this->container->get('cron')->run();
 
-    $node_storage = \Drupal::service('entity_type.manager')->getStorage('node');
-    $node = $node_storage->loadRevision($node_storage->getLatestRevisionId($node->id()));
+    $entity = $storage->loadRevision($storage->getLatestRevisionId($entity->id()));
 
     // Assert node is now published.
-    $this->assertEquals(TRUE, $node->isPublished());
-    $this->assertEquals('published', $node->moderation_state->value);
+    $this->assertEquals(TRUE, $entity->isPublished());
+    $this->assertEquals('published', $entity->moderation_state->value);
 
     // Assert only one revision is created during the operation.
-    $this->assertEquals($revision_id + 1, $node->getRevisionId());
+    $this->assertEquals($revision_id + 1, $entity->getRevisionId());
 
-    $node->setTitle('Draft title');
-    $node->moderation_state->value = 'draft';
-    $node->publish_on->value = strtotime('yesterday');
-    $node->publish_state->value = 'published';
-    $node->save();
+    $entity->set($titleField, 'Draft title');
+    $entity->moderation_state->value = 'draft';
+    $entity->publish_on->value = strtotime('yesterday');
+    $entity->publish_state->value = 'published';
+    $entity->save();
 
-    $node = $node_storage->loadRevision($node_storage->getLatestRevisionId($node->id()));
-    $this->assertEquals('Draft title', $node->getTitle());
-    $this->assertEquals('draft', $node->moderation_state->value);
+    $entity = $storage->loadRevision($storage->getLatestRevisionId($entity->id()));
+    $this->assertEquals('Draft title', $entity->label());
+    $this->assertEquals('draft', $entity->moderation_state->value);
 
     $this->container->get('cron')->run();
 
-    $node = $node_storage->loadRevision($node_storage->getLatestRevisionId($node->id()));
-    $this->assertEquals(TRUE, $node->isPublished());
-    $this->assertEquals('published', $node->moderation_state->value);
-    $this->assertEquals('Draft title', $node->getTitle());
+    // Assert that the entity is published after cron.
+    $entity = $storage->loadRevision($storage->getLatestRevisionId($entity->id()));
+    $this->assertEquals(TRUE, $entity->isPublished());
+    $this->assertEquals('published', $entity->moderation_state->value);
+    $this->assertEquals('Draft title', $entity->label());
   }
 
   /**
    * Tests moderated nodes unpublish scheduling.
+   *
+   * @dataProvider dataEntityTypes()
    */
-  public function testUnpublishStateSchedule() {
-    $node = Node::create([
+  public function testUnpublishStateSchedule($entityTypeId, $bundle) {
+    $storage = \Drupal::service('entity_type.manager')->getStorage($entityTypeId);
+
+    $entity = $this->createEntity($entityTypeId, $bundle, [
       'type' => 'example',
       'title' => 'Published title',
       'moderation_state' => 'published',
       'unpublish_on' => strtotime('yesterday'),
       'unpublish_state' => 'archived',
     ]);
-    $node->save();
-    $revision_id = $node->getRevisionId();
+    $entity_id = $entity->id();
+    $revision_id = $entity->getRevisionId();
 
-    // Make sure node is published.
-    $this->assertEquals(TRUE, Node::load($node->id())->isPublished());
+    // Make sure the entity is published.
+    $this->assertTrue($storage->load($entity_id)->isPublished(), 'The entity is published.');
 
     $this->container->get('cron')->run();
 
-    // Assert node is now unpublished.
-    $this->assertEquals(FALSE, Node::load($node->id())->isPublished());
+    // Assert entity is now unpublished.
+    $this->assertFalse($storage->load($entity_id)->isPublished(), 'The entity is unpublished after cron.');
 
     // Assert only one revision is created during the operation.
-    $this->assertEquals($revision_id + 1, Node::load($node->id())->getRevisionId());
+    $this->assertEquals($revision_id + 1, $storage->load($entity_id)->getRevisionId());
   }
 
   /**
    * Tests publish scheduling for a draft of a published node.
+   *
+   * @dataProvider dataEntityTypes()
    */
-  public function testPublishOfDraft() {
-    $node = Node::create([
+  public function testPublishOfDraft($entityTypeId, $bundle) {
+    $storage = \Drupal::service('entity_type.manager')->getStorage($entityTypeId);
+
+    $entity = $this->createEntity($entityTypeId, $bundle, [
       'type' => 'example',
       'title' => 'Published title',
       'moderation_state' => 'published',
     ]);
-    $node->save();
+    $entity_id = $entity->id();
 
-    $nid = $node->id();
+    // Assert entity is published.
+    $this->assertEquals('Published title', $storage->load($entity_id)->label());
+    $this->assertTrue($storage->load($entity_id)->isPublished(), 'Entity is initially published');
 
-    // Assert node is published.
-    $this->assertEquals('Published title', Node::load($nid)->getTitle());
-
-    // Create a new pending revision and validate it's not the deault published
+    // Create a new pending revision and validate it's not the default published
     // one.
-    $node->setTitle('Draft title');
-    $node->set('publish_on', strtotime('yesterday'));
-    $node->set('moderation_state', 'draft');
-    $node->set('publish_state', 'published');
-    $node->save();
-    $revision_id = $node->getRevisionId();
+    $titleField = ($entityTypeId == 'media') ? 'name' : 'title';
+    $entity->set($titleField, 'Draft title');
+    $entity->set('publish_on', strtotime('yesterday'));
+    $entity->set('moderation_state', 'draft');
+    $entity->set('publish_state', 'published');
+    $entity->save();
+    $revision_id = $entity->getRevisionId();
 
     // Test latest revision is not the published one.
-    $this->assertEquals('Published title', Node::load($nid)->getTitle());
+    $this->assertEquals('Published title', $storage->load($entity_id)->label());
 
     $this->container->get('cron')->run();
 
     // Test latest revision is now the published one.
-    $this->assertEquals('Draft title', Node::load($nid)->getTitle());
+    $this->assertEquals('Draft title', $storage->load($entity_id)->label());
 
     // Assert only one revision is created during the operation.
-    $this->assertEquals($revision_id + 1, Node::load($node->id())->getRevisionId());
+    $this->assertEquals($revision_id + 1, $storage->load($entity_id)->getRevisionId());
   }
 
 }
